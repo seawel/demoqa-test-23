212de04f54e1b54410a7f721baaa90db2faf29fb
Tatiana Romanovskaia <tvromanovskaia@gmail.com>
2025-10-14 17:58:55 +0300

    removed unused methods

--- src/test/java/SimpleJUnitTest.java
+++ src/test/java/SimpleJUnitTest.java
@@ -1,9 +1,5 @@
 import org.junit.jupiter.api.*;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
 public class SimpleJUnitTest {
 
     int result;
