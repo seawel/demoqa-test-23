80da67bb8b66b8ab028e6a73cbaea66c807c97d8
Tatiana Romanovskaia <tvromanovskaia@gmail.com>
2025-10-14 17:45:14 +0300

    added @BeforeEach

--- src/test/java/SimpleJUnitTest.java
+++ src/test/java/SimpleJUnitTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -6,19 +7,33 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SimpleJUnitTest {
+
+    int result;
+    
+    @BeforeEach
+    void beforEach()    {
+        System.out.println("###           beforEach()");
+       result = getResult();
+    }
     @Test
     void firstTest() {
-        Assertions.assertTrue(3 > 2);
+        int result = getResult();
+        System.out.println("###     firstTest()");
+        Assertions.assertTrue(result > 2);
     }
 
     @Test
     void secondTest() {
-        Assertions.assertTrue(3 > 2);
+        System.out.println("###     secondTest()");
+        Assertions.assertTrue(result > 2);
     }
 
     @Test
     void thirdTest() {
-        Assertions.assertTrue(3 > 2);
+        System.out.println("###     thirdTest()");
+        Assertions.assertTrue(result > 2);
     }
-
+      private int getResult() {
+        return 3;
+      }
 }
