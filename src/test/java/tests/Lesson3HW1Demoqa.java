2254ab78a67cfa7576030a78bf77b0989b647103
Tatiana Romanovskaia <tvromanovskaia@gmail.com>
2025-10-22 17:33:59 +0300

    added selectors up to "upload file"

--- src/test/java/tests/Lesson3HW1Demoqa.java
+++ src/test/java/tests/Lesson3HW1Demoqa.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Configuration.browserSize;
 import static com.codeborne.selenide.Configuration.config;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.files.DownloadActions.click;
 
 
 public class Lesson3HW1Demoqa {
@@ -22,11 +24,26 @@ public class Lesson3HW1Demoqa {
     @Test
     void fillPracticeFormTest() {
         open("/automation-practice-form");
-        $("#userName").setValue("Alex");
-        $("#userEmail").setValue("alex@egorov.com");
-        $("#currentAddress").setValue("Some street 1");
-        ///$("permanentAddress").setValue("Another street 1");
-        $("#submit").click();
+        $("#firstName").setValue("Alex");
+        $("#lastName").setValue("Pavlov");
+        $("#userEmail").setValue("alexpavlov@gmail.com");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("79156001234");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("March");
+        $(".react-datepicker__year-select").selectOption("1979");
+        $(".react-datepicker__day--022:not(.react-datepicker__day--outside-month)").click();
+
+        $("#subjectsInput").setValue("biology").pressEnter();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFromClasspath("now.jpg")
+
+
+
+
+
+
 
         $("#output #name").shouldHave(text("Alex"));
         $("#output #email").shouldHave(text("alex@egorov.com"));
