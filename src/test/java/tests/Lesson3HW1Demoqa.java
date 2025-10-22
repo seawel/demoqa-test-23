dcaa60ae9ca807e73dc6949dddd64a1d7bb2e70d
Tatiana Romanovskaia <tvromanovskaia@gmail.com>
2025-10-22 16:11:49 +0300

    added logs into build.gradle

---
+++ src/test/java/tests/Lesson3HW1Demoqa.java
@@ -0,0 +1,42 @@
+package tests;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.browserSize;
+import static com.codeborne.selenide.Configuration.config;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class Lesson3HW1Demoqa {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+
+    }
+
+    @Test
+    void fillPracticeFormTest() {
+        open("/automation-practice-form");
+        $("#userName").setValue("Alex");
+        $("#userEmail").setValue("alex@egorov.com");
+        $("#currentAddress").setValue("Some street 1");
+        ///$("permanentAddress").setValue("Another street 1");
+        $("#submit").click();
+
+        $("#output #name").shouldHave(text("Alex"));
+        $("#output #email").shouldHave(text("alex@egorov.com"));
+        $("#output #currentAddress").shouldHave(text("Some street 1"));
+        $("#output #permanentAddress").shouldHave(text("Alex"));
+
+
+
+
+    }
+}
+
+
