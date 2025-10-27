38e01bc44f49061831380f6bec18d123f9222df3
Tatiana Romanovskaia <tvromanovskaia@gmail.com>
2025-10-27 10:48:51 +0300

    added new file

---
+++ src/test/java/tests/RegistrationWithPageObjectsTests.java
@@ -0,0 +1,69 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class RegistrationWithPageObjectsTests {
+    package tests;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+    public class Lesson3HW1DemoqaTest {
+
+        @BeforeAll
+        static void beforeAll() {
+            Configuration.browserSize = "1920x1080";
+            Configuration.baseUrl = "https://demoqa.com";
+            Configuration.pageLoadStrategy = "eager";
+
+        }
+
+        @Test
+        void fillPracticeFormTest() {
+            open("/automation-practice-form");
+            $("#firstName").setValue("Alex");
+            $("#lastName").setValue("Pavlov");
+            $("#userEmail").setValue("alexpavlov@gmail.com");
+            $("#genterWrapper").$(byText("Male")).click();
+            $("#userNumber").setValue("79156001234");
+
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOption("March");
+            $(".react-datepicker__year-select").selectOption("1979");
+            $(".react-datepicker__day--022:not(.react-datepicker__day--outside-month)").click();
+
+            $("#subjectsInput").setValue("biology").pressEnter();
+            $("#hobbiesWrapper").$(byText("Music")).click();
+
+            $("#uploadPicture").uploadFromClasspath("cat.jpeg");
+            $("#currentAddress").setValue("Russia,630777,Moscow,ul.Pobedy,d.19,kv.6");
+            $("#state").click();
+            $("#state").$(byText("Rajasthan")).click();
+            $("#city").click();
+            $("#city").$(byText("Jaipur")).click();
+            $("#submit").click();
+
+            $(".modal-dialog").should(appear);
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form")));
+
+
+        }
+    }
+
+
+
+}
